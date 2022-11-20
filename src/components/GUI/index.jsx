8d4bd80c1388724d63ce2c1f2b01fb5c0001c988
VivianC <viviancao58@gmail.com>
2022-11-20 16:57:10 +1100

    create GUI component

---
+++ src/components/GUI/index.jsx
@@ -0,0 +1,12 @@
+import TodoForm from "../TodoForm";
+
+function GUI() {
+  return (
+    <div className="App">
+      <h1>A To-do List</h1>
+      <TodoForm />  
+    </div>
+  );
+}
+
+export default GUI;
