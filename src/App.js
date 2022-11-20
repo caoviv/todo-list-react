f3000e1ac082d1de52f1bc8d9cc6e186be1d27c4
VivianC <viviancao58@gmail.com>
2022-11-20 16:55:44 +1100

    moved html structure into a component

--- src/App.js
+++ src/App.js
@@ -1,31 +1,10 @@
 import "./App.css";
-import { useState } from "react";
+import GUI from "./components/GUI";
 
 function App() {
-  const [task, setTask] = useState ({
-    id: 0,
-    taskDescription: "",
-    isCompleted: false
-  });
   return (
-    <div className="App">
-      <h1>A To-do List</h1>
-      <form>
-        <div>
-          <label>
-            Task
-            <input
-              type="text"
-              value={task.taskDescription}
-              name="taskDescription"
-              onChange={(event) => setTask({ taskDescription: event.target.value, isCompleted: false})}
-            />
-          </label>
-          <button>Add Task</button>
-        </div>
-      </form>
-    </div>
+    <GUI />
   );
-}
+};
 
 export default App;
