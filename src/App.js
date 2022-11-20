69f81dee1469ca1d6a3ef107c14fdb4432be9ebb
VivianC <viviancao58@gmail.com>
2022-11-20 16:15:44 +1100

    create task useState hook

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [task, setTask] = useState ({
+    id: 0,
+    taskDescription: "",
+    isCompleted: false
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>A To-do List</h1>
+      <form>
+        <div>
+          <label>
+            Task
+            <input
+              type="text"
+              value={task.taskDescription}
+              name="taskDescription"
+              onChange={(event) => setTask({ taskDescription: event.target.value, isCompleted: false})}
+            />
+          </label>
+          <button>Add Task</button>
+        </div>
+      </form>
     </div>
   );
 }
